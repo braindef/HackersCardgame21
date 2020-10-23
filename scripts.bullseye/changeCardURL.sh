12eda3d010b43f7234a512cfb30d38deaa68a22d
Your Name <you@example.com>
2020-10-24 01:21:19 +0200

    corretions

--- scripts.bullseye/changeCardURL.sh
+++ scripts.bullseye/changeCardURL.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for i in $(find $1 -name '*.svg'); do sed -i 's/https:\/\/0x8.ch\/HackersCardgame19/https:\/\/0x8.ch\/HackersCardgame20/g' "$i"; done
+for i in $(find $1 -name '*.svg'); do sed -i 's/https:\/\/0x8.ch\/HackersCardgame20/https:\/\/0x8.ch\/HackersCardgame21/g' "$i"; done
